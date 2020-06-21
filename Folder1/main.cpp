7215f77a0cc36e94769e9ba142567bf584820668
Samarth Neema <neema.samarth.07@gmail.com>
2020-06-21 17:15:04 +0530

    ADd cpp file

---
+++ Folder1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum,n,i;
+    cout << "Enter a number " << endl;
+    cin >> n ;
+
+    for(i = 1; i<n ; i++)
+    {
+        sum = sum + i;
+    }
+    cout <<"sum is "<< sum;
+    return 0;
+}
